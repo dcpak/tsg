d077543cf120a53a372e1cd3c1e03e012b3eafa8
Przemysław Kasprzak <przemyslaw.kasprzak@orange.com>
2021-05-26 12:12:03 +0200

    Task 3 implementation - rejecting participants that have invalid e-mail.

--- src/Task.java
+++ src/Task.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Task {
@@ -8,6 +9,8 @@ public class Task {
      * Minimalny wiek dopuszczający uczestnika do konkursu (lata)
      */
     private static int MIN_AGE = 18;
+
+    private static final String REGEXP_EMAIL = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
     // don't modify main method
     public static void main(String[] args) {
@@ -36,11 +39,13 @@ public class Task {
      * @return Lista uczestników kwalifikujących się do konkursu
      */
     private static List<Person> getValidParticipants(List<Person> participants) {
+        Pattern patternEmail = Pattern.compile(REGEXP_EMAIL);
         List<Person> validParticipants = participants.stream()
                 .filter(participant ->
                         participant.getAge() >= MIN_AGE
                                 && Character.isUpperCase(participant.getName().charAt(0))
                                 && Character.isUpperCase(participant.getSurname().charAt(0))
+                                && patternEmail.matcher(participant.getEmail()).matches()
                 )
                 .collect(Collectors.toList());
         return validParticipants;
