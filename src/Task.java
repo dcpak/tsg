d94cddb59150d948b503579c15f60000660295de
Przemysław Kasprzak <przemyslaw.kasprzak@orange.com>
2021-05-26 12:23:20 +0200

    Implemented number of winners.

--- src/Task.java
+++ src/Task.java
@@ -10,6 +10,11 @@ public class Task {
      */
     private static int MIN_AGE = 18;
 
+    /**
+     * Liczba zwycięzców
+     */
+    public static final int NUMBER_OF_WINNERS = 3;
+
     private static final String REGEXP_EMAIL = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
     // don't modify main method
@@ -29,7 +34,7 @@ public class Task {
      */
     private static List<Person> andTheWinnersAre(List<Person> participants) {
         List<Person> validParticipants = getValidParticipants(participants);
-        return validParticipants;
+        return validParticipants.subList(0, NUMBER_OF_WINNERS);
     }
 
     /**
